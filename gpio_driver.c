9cdcc6dc873380ef84ea2169bf121347e1f7676c
francabrera0 <francabrera0@gmail.com>
2023-06-13 18:07:20 -0300

    litle changes

--- gpio_driver.c
+++ gpio_driver.c
@@ -8,7 +8,7 @@
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Tricore");
-MODULE_DESCRIPTION("A simple gpio driver for setting a LED and reading a button");
+MODULE_DESCRIPTION("Basic character device driver for raspi gpio");
 
 static dev_t my_device_nr;
 static struct class *my_class;
@@ -24,17 +24,12 @@ static ssize_t driver_read(struct file *File, char *user_buffer, size_t count, l
 	int to_copy, not_copied, delta;
 	char tmp[3] = " \n";
 
-	/* Get amount of data to copy */
 	to_copy = min(count, sizeof(tmp));
 
-	/* Read value of button */
-	printk("Value of button: %d\n", gpio_get_value(17));
 	tmp[0] = gpio_get_value(17) + '0';
 
-	/* Copy data to user */
 	not_copied = copy_to_user(user_buffer, &tmp, to_copy);
 
-	/* Calculate data */
 	delta = to_copy - not_copied;
 
 	return delta;
@@ -47,13 +42,10 @@ static ssize_t driver_write(struct file *File, const char *user_buffer, size_t c
 	int to_copy, not_copied, delta;
 	char value;
 
-	/* Get amount of data to copy */
 	to_copy = min(count, sizeof(value));
 
-	/* Copy data to user */
 	not_copied = copy_from_user(&value, user_buffer, to_copy);
 
-	/* Setting the LED */
 	switch(value) {
 		case '0':
 			gpio_set_value(4, 0);
@@ -66,9 +58,7 @@ static ssize_t driver_write(struct file *File, const char *user_buffer, size_t c
 			break;
 	}
 
-	/* Calculate data */
 	delta = to_copy - not_copied;
-
 	return delta;
 }
 
@@ -102,53 +92,44 @@ static struct file_operations fops = {
 static int __init ModuleInit(void) {
 	printk("Hello, Kernel!\n");
 
-	/* Allocate a device nr */
 	if( alloc_chrdev_region(&my_device_nr, 0, 1, DRIVER_NAME) < 0) {
 		printk("Device Nr. could not be allocated!\n");
 		return -1;
 	}
 	printk("read_write - Device Nr. Major: %d, Minor: %d was registered!\n", my_device_nr >> 20, my_device_nr && 0xfffff);
 
-	/* Create device class */
 	if((my_class = class_create(THIS_MODULE, DRIVER_CLASS)) == NULL) {
 		printk("Device class can not be created!\n");
 		goto ClassError;
 	}
 
-	/* create device file */
 	if(device_create(my_class, NULL, my_device_nr, NULL, DRIVER_NAME) == NULL) {
 		printk("Can not create device file!\n");
 		goto FileError;
 	}
 
-	/* Initialize device file */
 	cdev_init(&my_device, &fops);
 
-	/* Regisering device to kernel */
 	if(cdev_add(&my_device, my_device_nr, 1) == -1) {
 		printk("Registering of device to kernel failed!\n");
 		goto AddError;
 	}
 
-	/* GPIO 4 init */
 	if(gpio_request(4, "rpi-gpio-4")) {
 		printk("Can not allocate GPIO 4\n");
 		goto AddError;
 	}
 
-	/* Set GPIO 4 direction */
 	if(gpio_direction_output(4, 0)) {
 		printk("Can not set GPIO 4 to output!\n");
 		goto Gpio4Error;
 	}
 
-	/* GPIO 17 init */
 	if(gpio_request(17, "rpi-gpio-17")) {
 		printk("Can not allocate GPIO 17\n");
 		goto Gpio4Error;
 	}
 
-	/* Set GPIO 17 direction */
 	if(gpio_direction_input(17)) {
 		printk("Can not set GPIO 17 to input!\n");
 		goto Gpio17Error;
@@ -156,6 +137,7 @@ static int __init ModuleInit(void) {
 
 
 	return 0;
+	
 Gpio17Error:
 	gpio_free(17);
 Gpio4Error:
